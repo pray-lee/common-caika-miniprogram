209930fb99499ba72002ff84658d2d18531224d5
pray-lee <praycis@gmail.com>
2023-08-25 11:35:10 +0800

    udate

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ App({
     onLaunch(options) {
         // 获取设备信息
         const {model, environment} = wx.getSystemInfoSync()
-        const isIphoneXSeries = model.indexOf('iPhone X') !== -1
+        console.log(wx.getSystemInfoSync())
+        const isIphoneXSeries = model.indexOf('iPhone') !== -1
         this.globalData.isPhoneXSeries = isIphoneXSeries ? true : false
         this.globalData.isWxWork = environment ? true : false
     },
